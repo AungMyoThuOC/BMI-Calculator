49645ca503cbcc201abb1ad4160a84b6fc45cab3
AungMyoThuOC <thua40976@gmail.com>
2022-07-28 18:49:14 +0630

    bmi calculator

---
+++ lib/main.dart
@@ -0,0 +1,288 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'BMI Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const BMICalculator(),
+    );
+  }
+}
+
+class BMICalculator extends StatefulWidget {
+  const BMICalculator({Key? key}) : super(key: key);
+
+  @override
+  State<BMICalculator> createState() => _BMICalculatorState();
+}
+
+class _BMICalculatorState extends State<BMICalculator> {
+  int currentindex = 0;
+  String result = "";
+
+  // double _bmi = 0;
+  // String _textResult = "";
+
+  double height = 0;
+  double weight = 0;
+
+  TextEditingController heightController = TextEditingController();
+  TextEditingController weightController = TextEditingController();
+  TextEditingController ageController = TextEditingController();
+
+  // var item = ['Male', 'Female'];
+  // var dropdownValue = 1;
+
+  // String getInterpretation(BuildContext context) {
+  //   if (_bmi >= 30) {
+  //     _textResult = AppLocalizations.of(context).obeseWeight;
+  //   } else if (_bmi >= 25) {
+  //     _textResult = AppLocalizations.of(context).overWeight;
+  //   } else if (_bmi >= 18.5) {
+  //     _textResult = AppLocalizations.of(context).normalWeight;
+  //   } else {
+  //     _textResult = AppLocalizations.of(context).underWeight;
+  //   }
+  //  return _textResult;
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("BMI Calculator"),
+          elevation: 0,
+          backgroundColor: Colors.white,
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(children: const [
+                  Icon(
+                    Icons.person,
+                    size: 60,
+                  ),
+                  Text(
+                    "Age : ",
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                ]),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  controller: ageController,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                      hintText: "Your Age ",
+                      filled: true,
+                      fillColor: Colors.grey[200],
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                          borderSide: BorderSide.none)),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(children: const [
+                  Icon(
+                    Icons.elevator,
+                    size: 60,
+                  ),
+                  Text(
+                    "Your Height in Cm : ",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )
+                ]),
+                const SizedBox(
+                  height: 8,
+                ),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  controller: heightController,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                      hintText: "Your Height in Cm",
+                      filled: true,
+                      fillColor: Colors.grey[200],
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                          borderSide: BorderSide.none)),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                // ignore: prefer_const_constructors
+                Row(
+                  children: [
+                    IconButton(
+                        onPressed: () {},
+                        icon: const FaIcon(
+                          FontAwesomeIcons.weightScale,
+                          size: 40,
+                        )),
+                    const Text(
+                      "      Your Weight in kg : ",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  controller: weightController,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                      hintText: "Your Weight in kg",
+                      filled: true,
+                      fillColor: Colors.grey[200],
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                          borderSide: BorderSide.none)),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                        onPressed: () {},
+                        icon: const FaIcon(FontAwesomeIcons.venusMars)),
+                    // DropdownButton(
+                    //   value: dropdownValue,
+                    //   items: const [
+                    //     DropdownMenuItem(
+                    //       value: 1,
+                    //       child: Text("Male"),
+                    //     ),
+                    //     DropdownMenuItem(
+                    //       value: 2,
+                    //       child: Text("Female"),
+                    //     )
+                    //   ],
+                    //   onChanged: (newValue) {
+                    //     setState(() {
+                    //       dropdownValue = int.parse(newValue.toString());
+                    //     });
+                    //   },
+                    // ),
+                    radioButton("Male", Colors.blue, 0),
+                    radioButton("Female", Colors.pink, 1),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 50,
+                  child: FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        height = double.parse(heightController.value.text);
+                        weight = double.parse(weightController.value.text);
+                      });
+                      calculatorBmi(height, weight);
+                    },
+                    color: Colors.blue,
+                    child: const Text(
+                      "Calculator",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: double.infinity,
+                  child: const Text(
+                    "Your BMI is :",
+                    textAlign: TextAlign.center,
+                    style:
+                        TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                Container(
+                  width: double.infinity,
+                  child: Text(
+                    "$result",
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void calculatorBmi(double height, double weight) {
+    double finalresult = weight / (height * height / 10000);
+    String bmi = finalresult.toStringAsFixed(2);
+    setState(() {
+      result = bmi;
+    });
+  }
+
+  void changeIndex(int index) {
+    setState(() {
+      currentindex = index;
+    });
+  }
+
+  Widget radioButton(String value, Color color, int index) {
+    return Expanded(
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 12.0),
+        height: 80,
+        child: FlatButton(
+          color: currentindex == index ? color : Colors.grey[200],
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          onPressed: () {
+            changeIndex(index);
+          },
+          child: Text(
+            value,
+            style: TextStyle(
+              color: currentindex == index ? Colors.white : color,
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
